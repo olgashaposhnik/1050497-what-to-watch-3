62dc880a2cabe9892c802ed516b9c3e654f46c65
olgashaposhnik <50341504+olgashaposhnik@users.noreply.github.com>
2020-02-07 10:46:24 +0300

    выполнила задание 1-2

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require(`path`);
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    open: true, // чтоб вебпак не открывал ссылку на будущий сайт или наоборот, открывал
+    inline: true, // перезагрузка страницы при внесении изменений
+    port: 1337,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
+      }
+    ],
+  },
+  devtool: `source-map`,
+};
