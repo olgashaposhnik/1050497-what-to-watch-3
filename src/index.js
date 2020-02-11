cff3f9899383d8a01337db35af457cd09f59de3c
olgashaposhnik <50341504+olgashaposhnik@users.noreply.github.com>
2020-02-11 17:11:58 +0300

    выполнила задание 2-1

--- src/index.js
+++ src/index.js
@@ -6,6 +6,28 @@ const FilmData = {
   NAME: `The Grand Budapest Hotel`,
   GENRE: `Drama`,
   YEAR: 2014,
+  TITLES: [`The Grand Budapest Hotel`,
+    `Fantastic Beasts: The Crimes of Grindelwald`,
+    `Bohemian Rhapsody`,
+    `Macbeth`,
+    `Aviator`,
+    `We need to talk about Kevin`,
+    `What We Do in the Shadows`,
+    `Revenant`,
+    `Johnny English`,
+    `Shutter Island`,
+    `Pulp Fiction`,
+    `No Country for Old Men`,
+    `Snatch`,
+    `Moonrise Kingdom`,
+    `Seven Years in Tibet`,
+    `Midnight Special`,
+    `War of the Worlds`,
+    `Dardjeeling Limited`,
+    `Orlando`,
+    `Mindhunter`,
+    `Midnight Special`
+  ],
 };
 
 ReactDOM.render(
@@ -13,6 +35,8 @@ ReactDOM.render(
       name={FilmData.NAME}
       genre={FilmData.GENRE}
       year={FilmData.YEAR}
+      titles={FilmData.TITLES}
     />,
     document.querySelector(`#root`)
 );
+
