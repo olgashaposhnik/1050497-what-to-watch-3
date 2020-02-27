f9fbcc14923b483f6978b8b4f80d336efa9c2fb5
olgashaposhnik <50341504+olgashaposhnik@users.noreply.github.com>
2020-02-27 11:30:57 +0300

    выполнила задание 3-2

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,19 +1,70 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import Main from "../main/main.jsx";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import FilmDetails from "../film-details/film-details.jsx";
 
-const titleLinkClickHandler = (evt) => {
-  evt.preventDefault();
-};
+const filmCardHoverHandler = () => {};
 
-const App = (props) => {
-  const {name, genre, year, films} = props;
-  return (
-    <Main name={name} genre={genre} year={year} films={films}
-      onTitleLinkClick={titleLinkClickHandler}
-    />
-  );
-};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeCard: null
+    };
+
+    this._onFilmCardClickHandler = this._onFilmCardClickHandler.bind(this);
+  }
+
+  _onFilmCardClickHandler(card) {
+    this.setState({activeCard: card});
+  }
+
+  _renderMain() {
+    const {name, genre, year, films, filmDetails} = this.props;
+    const {activeCard} = this.state;
+
+    if (!activeCard) {
+      return (
+        <Main
+          name={name}
+          genre={genre}
+          year={year}
+          films={films}
+          onFilmCardClick={this._onFilmCardClickHandler}
+          onFilmCardHover={filmCardHoverHandler}/>
+      );
+    }
+
+    if (activeCard) {
+      return (
+        <FilmDetails
+          filmDetails={filmDetails}
+        />
+      );
+    }
+    return null;
+  }
+
+  render() {
+    const {filmDetails} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderMain()}
+          </Route>
+          <Route exact path="//dev-film"> // проверить это!!!
+            <FilmDetails
+              filmDetails={filmDetails}/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   name: PropTypes.string.isRequired,
@@ -24,7 +75,17 @@ App.propTypes = {
         image: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired
       }).isRequired
-  ).isRequired
+  ).isRequired,
+  filmDetails: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    fullImage: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+    starring: PropTypes.arrayOf(PropTypes.string).isRequired,
+    duration: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+  }).isRequired
 };
 
 export default App;
---
+++ src/components/film-details/film-details.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const FilmDetails = (props) => {
+  const {film} = props;
+  const {title, image, fullImage, director, starring, duration, genre, year} = film;
+  return (
+    <section className="movie-card movie-card--full">
+      <div className="movie-card__hero">
+        <div className="movie-card__bg">
+          <img src={fullImage} alt={title} />
+        </div>
+        <h1 className="visually-hidden">WTW</h1>
+        <header className="page-header movie-card__head">
+          <div className="logo">
+            <a href="main.html" className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+          <div className="user-block">
+            <div className="user-block__avatar">
+              <img src="img/avatar.jpg" alt="User avatar" width={63} height={63} />
+            </div>
+          </div>
+        </header>
+        <div className="movie-card__wrap">
+          <div className="movie-card__desc">
+            <h2 className="movie-card__title">{title}</h2>
+            <p className="movie-card__meta">
+              <span className="movie-card__genre">{genre}</span>
+              <span className="movie-card__year">{year}</span>
+            </p>
+            <div className="movie-card__buttons">
+              <button className="btn btn--play movie-card__button" type="button">
+                <svg viewBox="0 0 19 19" width={19} height={19}>
+                  <use xlinkHref="#play-s" />
+                </svg>
+                <span>Play</span>
+              </button>
+              <button className="btn btn--list movie-card__button" type="button">
+                <svg viewBox="0 0 19 20" width={19} height={20}>
+                  <use xlinkHref="#add" />
+                </svg>
+                <span>My list</span>
+              </button>
+              <a href="add-review.html" className="btn movie-card__button">Add review</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="movie-card__wrap movie-card__translate-top">
+        <div className="movie-card__info">
+          <div className="movie-card__poster movie-card__poster--big">
+            <img src={image} alt={title} width={218} height={327} />
+          </div>
+          <div className="movie-card__desc">
+            <nav className="movie-nav movie-card__nav">
+              <ul className="movie-nav__list">
+                <li className="movie-nav__item">
+                  <a href="#" className="movie-nav__link">Overview</a>
+                </li>
+                <li className="movie-nav__item movie-nav__item--active">
+                  <a href="#" className="movie-nav__link">Details</a>
+                </li>
+                <li className="movie-nav__item">
+                  <a href="#" className="movie-nav__link">Reviews</a>
+                </li>
+              </ul>
+            </nav>
+            <div className="movie-card__text movie-card__row">
+              <div className="movie-card__text-col">
+                <p className="movie-card__details-item">
+                  <strong className="movie-card__details-name">Director</strong>
+                  <span className="movie-card__details-value">{director}</span>
+                </p>
+                <p className="movie-card__details-item">
+                  <strong className="movie-card__details-name">Starring</strong>
+                  <span className="movie-card__details-value">
+                    {starring.map((actor, index) => (
+                      <React.Fragment key={actor + index}>
+                        {actor} <br />
+                      </React.Fragment>
+                    ))}
+                  </span>
+                </p>
+              </div>
+              <div className="movie-card__text-col">
+                <p className="movie-card__details-item">
+                  <strong className="movie-card__details-name">Run Time</strong>
+                  <span className="movie-card__details-value">{duration}</span>
+                </p>
+                <p className="movie-card__details-item">
+                  <strong className="movie-card__details-name">Genre</strong>
+                  <span className="movie-card__details-value">{genre}</span>
+                </p>
+                <p className="movie-card__details-item">
+                  <strong className="movie-card__details-name">Released</strong>
+                  <span className="movie-card__details-value">{year}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+
+FilmDetails.propTypes = {
+  film: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    fullImage: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+    starring: PropTypes.arrayOf(PropTypes.string).isRequired,
+    duration: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+  }).isRequired
+};
+
+export default FilmDetails;
